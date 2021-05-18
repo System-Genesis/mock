a32b88387ea140bfa1098a2f84a9c98e62c075ad
menahem <mnjol770@gmail.com>
2021-05-18 10:37:01 +0300

    feat: get files from last version. chenge file to ts

---
+++ src/lists/dataTypesList.ts
@@ -0,0 +1,40 @@
+const fn = require("../../src/config/fieldNames");
+
+export default {
+  RANK: ["unknown", "rookie", "champion", "ultimate", "mega"],
+
+  CURRENT_UNIT: ["nitro unit", "jelly unit"],
+  ENTITY_TYPE: ["digimon", "agumon", "tamar"],
+  RESPONSIBILITY: ["none", "HR", "SO"],
+  STATUS: ["active", "inactive"],
+
+  DOMAIN_MAP: [
+    ["rabiran.com", "rabiranuid"],
+    ["somedomain.com", "somedomainuid"],
+    ["jello.com", "jellouid"],
+    ["jello2.com", "jellouid"],
+    ["turtle.com", "turtle"],
+    ["donatelo.turtle.com", "turtle"],
+    ["rafael.turtle.com", "turtle"],
+    ["leonardo.com", "ddd"],
+  ],
+
+  SERVICE_TYPE: ["A", "B", "C", "D", "E", "F", "G", "H"],
+  UNIT: ["gondor", "mordor", "wallmart", "valhalla"],
+  ES_UNIT: ["es1", "es2", "es3", "es4", "es5", "es6"],
+  ADS_UNIT: ["ads1", "ads2", "ads3", "ads4", "ads5", "ads6"],
+  CITY_UNIT: ["city1", "city2", "city3", "city4", "city5", "city6"],
+  MM_UNIT: ["mm1", "mm2", "mm3", "mm4", "mm5", "mm6"],
+  MIRI_TAGS: [
+    {
+      name: fn[fn.dataSources.city].userTags.transportable,
+    },
+    {
+      name: fn[fn.dataSources.city].userTags.information,
+    },
+  ],
+  MIRI_DOMAINS: [
+    fn[fn.dataSources.city].domainNames.internal,
+    fn[fn.dataSources.city].domainNames.external,
+  ],
+};
---
+++ src/utils.ts
@@ -0,0 +1,42 @@
+const faker = require("faker");
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * Math.floor(max - min + 1) + min);
+}
+
+const utils = {
+  createCheckDigit: (param) => {
+    const rawCheckDigit = param
+      .toString()
+      .split("")
+      .reduce((accumulator, currChar, currIndex) => {
+        let digitWeight = Number(currChar) * ((currIndex % 2) + 1);
+
+        return (accumulator += digitWeight > 9 ? digitWeight - 9 : digitWeight);
+      }, 0);
+
+    return rawCheckDigit % 10 ? 10 - (rawCheckDigit % 10) : 0;
+  },
+
+  randomElement: (array) => {
+    return array[Math.floor(Math.random() * array.length)];
+  },
+  randomArrFromArr: (array) => {
+    const n = getRandomInt(1, array.length);
+    return array.sort(() => Math.random() - Math.random()).slice(0, n);
+  },
+  generateID: () => {
+    const tz = faker.datatype
+      .number({ min: 10000000, max: 99999999 })
+      .toString();
+    return tz + utils.createCheckDigit(tz);
+  },
+  generateNumberBody: () => {
+    return faker.datatype.number({ min: 1000000, max: 9999999 }).toString();
+  },
+  generateNumberPrefix: () => {
+    return faker.datatype.number({ min: 50, max: 59 }).toString();
+  },
+};
+
+export default utils;
