5493d2d2f318e5fa96c71daff0387bbd31d96f39
tommyafek <tommyafek@gmail.com>
2021-05-24 16:03:24 +0300

    fix: auth error msg as func to call only on error

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,25 +1,25 @@
-import jwt from 'jsonwebtoken';
-import fs from 'fs';
-import path from 'path';
-import { NextFunction, Request, Response } from 'express';
+import jwt from "jsonwebtoken";
+import fs from "fs";
+import path from "path";
+import { NextFunction, Request, Response } from "express";
+
+const errorRes = (res: Response) => res.status(401).send("Unauthorized");
 
 export const isAuth = async (
   req: Request,
-  response: Response,
+  res: Response,
   next: NextFunction,
   myToken: string
 ) => {
-  const errorRes = response.status(401).send('Unauthorized');
-
-  const token = req.header('Authorization');
-  const key = fs.readFileSync(path.join(__dirname, '../config/key.pem'));
+  const token = req.header("Authorization");
+  const key = fs.readFileSync(path.join(__dirname, "../config/key.pem"));
 
   try {
-    if (!token) return errorRes;
+    if (!token) return errorRes(res);
 
     const payload: any = jwt.verify(token, key.toString());
 
-    if (!payload || payload.aud !== myToken) return errorRes;
+    if (!payload || payload.aud !== myToken) return errorRes(res);
 
     return next();
   } catch (err) {
