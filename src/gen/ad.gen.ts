125a7b09cecb3b14a1c933708c48a473067349c8
menahem <mnjol770@gmail.com>
2021-05-27 10:47:17 +0300

    feat: add "d" and "bb" to ad

--- src/gen/ad.gen.ts
+++ src/gen/ad.gen.ts
@@ -1,35 +1,36 @@
-import { adUser } from "../types/types";
-import faker from "faker";
-import dataTypes from "../lists/dataOption";
+import { adUser } from '../types/types';
+import faker from 'faker';
+import dataTypes from '../lists/dataOption';
+import utils from '../utils/utils';
 
-export function createAdUser(
-  firstName: string,
-  lastName: string,
-  userPrincipalName: string
-) {
+export function createAdUser(firstName: string, lastName: string, mi: string) {
   const job = faker.name.jobTitle();
-  const sAMAccountName = faker.internet.email().split("@")[0];
+  const sAMAccountName = faker.internet.email().split('@')[0];
 
   let ad: adUser = {
     KfirstName: firstName,
     guName: firstName,
     KlastName: lastName,
-    userPrincipalName: "M" + userPrincipalName,
+    userPrincipalName: utils.randomElement([
+      'M' + mi,
+      'D' + utils.generateID(),
+      'BB' + utils.generateID(),
+    ]),
     Kjob: job,
     hierarchy:
       faker.lorem.word() +
-      "/" +
+      '/' +
       faker.lorem.word() +
-      "/" +
+      '/' +
       faker.lorem.word() +
-      "/" +
+      '/' +
       job +
-      " - " +
+      ' - ' +
       firstName +
-      " " +
+      ' ' +
       lastName,
     sAMAccountName: sAMAccountName,
-    mail: sAMAccountName + "@" + dataTypes.DOMAIN_MAP[0][0],
+    mail: sAMAccountName + '@' + dataTypes.DOMAIN_MAP[0][0],
   };
 
   return ad;
